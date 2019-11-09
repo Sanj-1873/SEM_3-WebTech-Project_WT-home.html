1119fe942f487bd28fce155d5d931a7cbbdc5e85
hat-im <hatim3121@gmail.com>
2019-11-10 02:31:15 +0530

    Got pie charts

--- script_party-compare.js
+++ script_party-compare.js
@@ -1,63 +1,54 @@
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart', "bar"]});
 google.charts.setOnLoadCallback(drawCharts);
 function drawPieChart(element, data, options){
     var chart = new google.visualization.PieChart(element);
     chart.draw(data, options);
 }
 
-function drawColumnChart(element, data){
+function drawColumnChart(element, data, options){
     var chart = new google.charts.Bar(element);
-    var dat=google.visualization.arrayToDataTable(data)
-    chart.draw()
+    chart.draw(data, options);
 }
 
 function drawCharts(){
     var pie_arr=document.getElementsByClassName("pie");
-    var col_arr=document.getElementsByClassName("bar");
+    var col_arr=document.getElementsByClassName("col");
 
     var pie_data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Work', 8],
-        ['Friends', 2],
-        ['Eat', 2],
-        ['TV', 2],
-        ['Gym', 2],
-        ['Sleep', 8]
+        ['Area', 'Money'],
+        ['Education', 8],
+        ['Infra', 2],
+        ['Millitary', 2],
+        ['Agriculture', 2],
+        ['Automobile', 2],
+        ['Telecom', 8]
     ]);
-    var pie_options = {'title':'My Average Day', 'width':550, 'height':400};
+    var pie_options = {'title':'Party A', 'width':550, 'height':400};
 
-    var col_data = [
-        ['Element', 'Density', { role: 'style' }, { role: 'annotation' } ],
-        ['Copper', 8.94, '#b87333', 'Cu' ],
-        ['Silver', 10.49, 'silver', 'Ag' ],
-        ['Gold', 19.30, 'gold', 'Au' ],
-        ['Platinum', 21.45, 'color: #e5e4e2', 'Pt' ]];
+    var col_data = new google.visualization.arrayToDataTable([
+        ['Year', 'Amount Speent'],
+        ["2018", 44],
+        ["2013", 31],
+        ["2008", 12],
+        ["2005", 10]
+      ]);
 
-    var col_options = {
-        chart: {
-          title: 'Population of Largest U.S. Cities',
-          subtitle: 'Based on most recent and previous census data'
-        },
-        hAxis: {
-          title: 'Total Population'
-        },
-        vAxis: {
-          title: 'City'
-        },
-        bars: 'horizontal',
-        series: {
-            0: {axis: '2010'},
-            1: {axis: '2000'}
-        },
+      var col_options = {
+        title: 'Money by year',
+        width: 900,
+        legend: { position: 'none' },
+        chart: { title: 'Money by year',
+                 subtitle: 'by government' },
+        bars: 'horizontal', // Required for Material Bar Charts.
         axes: {
-            x: {
-                2010: {label: '2010 Population (in millions)', side: 'top'},
-                2000: {label: '2000 Population'}
-            }
-        }
-    };
+          x: {
+            0: { side: 'top', label: 'Percentage'} // Top x-axis.
+          }
+        },
+        bar: { groupWidth: "90%" }
+      };
     for(i=0;i<pie_arr.length;i++)
         drawPieChart(pie_arr[i], pie_data, pie_options);
-    for(i=0; i<bar_ray.length; i++)
-        drawColumnChart(col_arr[i], col_data, col_ptions)
+    for(i=0; i<col_arr.length; i++)
+        drawColumnChart(col_arr[i], col_data, col_options)
 }
