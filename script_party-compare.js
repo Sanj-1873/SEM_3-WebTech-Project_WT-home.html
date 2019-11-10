4197f47eb7ae8431ef55dc8f6490e4d03378f80e
Trinity01873 <39741031+Trinity01873@users.noreply.github.com>
2019-11-10 17:50:50 +0530

    Add files via upload
    Added styling to the page 
    we still need to mirror it 
    so its half done

--- script_party-compare.js
+++ script_party-compare.js
@@ -1,91 +1,25 @@
-google.charts.load('current', {'packages':['corechart', "bar"]});
-google.charts.setOnLoadCallback(drawCharts);
-function drawPieChart(element, data, options){
-    var chart = new google.visualization.PieChart(element);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart(elem){
+    var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work', 8],
+    ['Friends', 2],
+    ['Eat', 2],
+    ['TV', 2],
+    ['Gym', 2],
+    ['Sleep', 8]
+  ]);
+
+    // Optional; add a title and set the width and height of the chart
+    var options = {'title':'My Average Day', 'width':550, 'height':400};
+
+    // Display the chart inside the <div> element with id="piechart"
+    var chart = new google.visualization.PieChart(elem);
     chart.draw(data, options);
 }
 
-function drawBarChart(element, data, options){
-    var chart = new google.visualization.ColumnChart(element);
-    chart.draw(data, options);
-}
+var pie_arr=document.getElementsByClassName("pie");
+var bar_ray=document.getElementsByClassName("bar");
 
-function drawCharts(){
-    var pie_arr=document.getElementsByClassName("pie");
-    var bar_arr=document.getElementsByClassName("col");
-
-    var pie_data = [
-        google.visualization.arrayToDataTable([
-            ['Area', 'Money'],
-            ['Education', 8],
-            ['Infra', 2],
-            ['Millitary', 2],
-            ['Agriculture', 2],
-            ['Automobile', 2],
-            ['Telecom', 8]
-        ]),
-        google.visualization.arrayToDataTable([
-            ['Area', 'Money'],
-            ['Education', 4],
-            ['Infra', 3],
-            ['Millitary', 7],
-            ['Agriculture', 6],
-            ['Automobile', 3],
-            ['Telecom', 5]
-        ]),
-    ];
-    var pie_options = {
-        title:'Party A', 
-        width:550, 
-        height:400, 
-        backgroundColor: {fill: 'transparent'},
-        animation:{
-            startup: true,
-          duration: 1000,
-          easing: 'out',
-        }
-    };
-
-    var bar_data = [
-        new google.visualization.arrayToDataTable([
-            ['Year', 'Amount Speent'],
-            ["2018", 44],
-            ["2013", 31],
-            ["2008", 12],
-            ["2005", 10],
-            ["2023", 14]
-        ]),
-        new google.visualization.arrayToDataTable([
-            ['Year', 'Amount Speent'],
-            ["2018", 34],
-            ["2013", 37],
-            ["2008", 32],
-            ["2005", 30],
-            ["2023", 41]
-        ]),
-        new google.visualization.arrayToDataTable([
-            ['Year', 'Amount Speent'],
-            ["2018", 64],
-            ["2013", 21],
-            ["2008", 72],
-            ["2005", 80],
-            ["2023", 81]
-        ]),
-    ];
-
-      var col_options = {
-        width: 250,
-        height: 300,
-        backgroundColor: {fill: 'transparent'},
-        animation:{
-            startup: true,
-          duration: 1000,
-          easing: 'out',
-        },
-        vAxis: {minValue:0, maxValue:100},
-      };
-    for(i=0;i<pie_arr.length;i++)
-        drawPieChart(pie_arr[i], pie_data[i], pie_options);
-    for(i=0; i<bar_arr.length; i++)
-        drawBarChart(bar_arr[i], bar_data[i], col_options)
-}
+for(i=0;i<pie_arr.length;i++) 
