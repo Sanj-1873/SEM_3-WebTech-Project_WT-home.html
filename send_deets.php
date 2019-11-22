834df7c3740562e378811388258875d64226376d
hat-im <hatim3121@gmail.com>
2019-11-22 14:30:42 +0530

    Deleted Seperate state pages

--- send_deets.php
+++ send_deets.php
@@ -4,18 +4,21 @@
 <h1>My first PHP page</h1>
 
 <?php
-$inputFileType = 'Excel2007';
-$inputFileName = 'data.xlsx';
-$objReader = PHPExcel_IOFactory::createReader($inputFileType);
-$worksheetNames = $objReader->listWorksheetNames($inputFileName);
-
-foreach ($worksheetNames as $sheetName) {
-    //BELOW IS "WISH CODE":
-    foreach($row = 1; $row <=$max_rows; $row+= 100) {
-        $dataset = $objReader->getWorksheetWithRows($row, $row+100);
-        save_dataset_to_database($dataset);
+$foundInCells = array();
+$searchValue = 'USA';
+foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+    $ws = $worksheet->getTitle();
+    foreach ($worksheet->getRowIterator() as $row) {
+        $cellIterator = $row->getCellIterator();
+        $cellIterator->setIterateOnlyExistingCells(true);
+        foreach ($cellIterator as $cell) {
+            if ($cell->getValue() == $searchValue) {
+                $foundInCells[] = $ws . '!' . $cell->getCoordinate();
+            }
+        }
     }
 }
+var_dump($foundInCells);
 ?>
 
 </body>
